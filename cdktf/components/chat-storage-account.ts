50036b17125a12789a6b7abd0fd3de779271caff
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2023-04-20 08:38:09 +0000

    Add screen share storage.

--- cdktf/components/chat-storage-account.ts
+++ cdktf/components/chat-storage-account.ts
@@ -6,59 +6,92 @@ import { StorageTable } from "@cdktf/provider-azurerm/lib/storage-table";
 import { Construct } from "constructs";
 
 export interface ChatStorageAccountProps {
-    uniquePrefix: string;
-    resourceGroup: ResourceGroup;
+  uniquePrefix: string;
+  resourceGroup: ResourceGroup;
 }
 
 export class ChatStorageAccountConstruct extends Construct {
-    public readonly chatStorageAccount: StorageAccount;
+  public readonly chatStorageAccount: StorageAccount;
 
 
-    constructor(scope: Construct, id: string, props: ChatStorageAccountProps) {
-        super(scope, id);
-        this.chatStorageAccount = new StorageAccount(this, "chatStorageAccount", {
-            name: props.uniquePrefix + "storageaccount",
-            resourceGroupName: props.resourceGroup.name,
-            location: props.resourceGroup.location,
-            accountTier: "Standard",
-            accountReplicationType: "LRS",
-            staticWebsite: {
-                indexDocument: "index.html",
-                error404Document: "404.html"
-            }
-        })
+  constructor(scope: Construct, id: string, props: ChatStorageAccountProps) {
+    super(scope, id);
+    this.chatStorageAccount = new StorageAccount(this, "chatStorageAccount", {
+      name: props.uniquePrefix + "storageaccount",
+      resourceGroupName: props.resourceGroup.name,
+      location: props.resourceGroup.location,
+      accountTier: "Standard",
+      accountReplicationType: "LRS",
+      staticWebsite: {
+        indexDocument: "index.html",
+        error404Document: "404.html"
+      }
+    });
 
-        new StorageTable(this, "chatStorageTable", {
-            name: "chatHistory",
-            storageAccountName: this.chatStorageAccount.name,
-        })
+    new StorageTable(this, "chatStorageTable", {
+      name: "chatHistory",
+      storageAccountName: this.chatStorageAccount.name,
+    });
 
-        new StorageTable(this, "userStorageTable", {
-            name: "users",
-            storageAccountName: this.chatStorageAccount.name,
-        })
+    new StorageTable(this, "userStorageTable", {
+      name: "users",
+      storageAccountName: this.chatStorageAccount.name,
+    });
 
-        new StorageContainer(this, "chatStorageBlob", {
-            name: "voice",
-            storageAccountName: this.chatStorageAccount.name,
-            containerAccessType: "blob",
-        })
+    new StorageTable(this, "classesStorageTable", {
+      name: "classes",
+      storageAccountName: this.chatStorageAccount.name,
+    });
 
-        new StorageManagementPolicy(this, "StorageManagementPolicy", {
-            storageAccountId: this.chatStorageAccount.id,
-            rule: [{
-                name: "DeleteOldBlobs",
-                enabled: true,
-                filters: {
-                    blobTypes: ["blockBlob"],
-                    prefixMatch: ["voice/*"]
-                },
-                actions: {
-                    baseBlob: {
-                        deleteAfterDaysSinceModificationGreaterThan: 1
-                    }
-                }
-            }]
-        });
-    }
+    new StorageTable(this, "sessionsStorageTable", {
+      name: "sessions",
+      storageAccountName: this.chatStorageAccount.name,
+    });
+
+    new StorageContainer(this, "chatStorageBlob", {
+      name: "voice",
+      storageAccountName: this.chatStorageAccount.name,
+      containerAccessType: "blob",
+    });
+
+    new StorageManagementPolicy(this, "StorageManagementPolicy", {
+      storageAccountId: this.chatStorageAccount.id,
+      rule: [{
+        name: "DeleteOldBlobs",
+        enabled: true,
+        filters: {
+          blobTypes: ["blockBlob"],
+          prefixMatch: ["voice/*"]
+        },
+        actions: {
+          baseBlob: {
+            deleteAfterDaysSinceModificationGreaterThan: 1
+          }
+        }
+      }]
+    });
+
+    new StorageContainer(this, "screenStorageBlob", {
+      name: "screen",
+      storageAccountName: this.chatStorageAccount.name,
+      containerAccessType: "blob",
+    });
+
+    new StorageManagementPolicy(this, "screenBlobStorageManagementPolicy", {
+      storageAccountId: this.chatStorageAccount.id,
+      rule: [{
+        name: "DeleteOldBlobs",
+        enabled: true,
+        filters: {
+          blobTypes: ["blockBlob"],
+          prefixMatch: ["screen/*"]
+        },
+        actions: {
+          baseBlob: {
+            deleteAfterDaysSinceModificationGreaterThan: 7
+          }
+        }
+      }]
+    });
+  }
 }
